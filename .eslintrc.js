c32498fe817ec4552f0c76074fabab9dabbed9b3
Dmitry Sorin <dsorin@yandex-team.ru>
2016-09-19 12:37:05 +0300

    Add project stub files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1}
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # chr-chr-chr
+Chromium extension for seamless node debug
+
+## Develop
+```bash
+$ # clone repo
+$ git clone git@github.yandex-team.ru:kino/chr-chr-chr.git
+$ cd chr-chr-chr
+$ nvm use # in case you're osx user
+$ npm install
+```
